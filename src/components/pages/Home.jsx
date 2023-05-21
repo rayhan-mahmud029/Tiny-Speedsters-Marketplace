22844f1e834d3c69ecdd9b35b0b712b14db241f6
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-05-21 14:30:07 +0600

    react tabs added in the category section

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 const Home = () => {
     return (
@@ -21,12 +22,91 @@ const Home = () => {
             {/* feature's */}
             <section>
                 <h1 className='text-2xl lg:text-5xl text-center my-6'>Featured Products</h1>
-                <div className='grid grid-cols-2 gap-2 lg:gap-4'>
-                    <img src="https://i.ibb.co/JnDrMQW/pexels-nubia-navarro-nubikini-1522185.jpg" alt="" className="object-cover h-64 rounded-lg shadow-md w-full" />
-                    <img src="https://i.ibb.co/0yRVQ0s/pexels-pixabay-35967.jpg" alt="" className="object-cover h-64 rounded-lg shadow-md w-full" />
-                    <img src="https://i.ibb.co/k6zDM9t/pexels-hosein-ashrafosadat-243206.jpg" alt="" className="object-cover h-64 rounded-lg shadow-md w-full" />
-                    <img src="https://i.ibb.co/RywSDCk/pexels-pixabay-35619.jpg" alt="" className="object-cover h-64 rounded-lg shadow-md w-full" />
+                <div className="grid grid-cols-3 gap-4">
+                    <div className="col-span-1">
+                        <img
+                            src="https://i.ibb.co/8j7hVjv/toy-7796918.jpg"
+                            alt="Photo 1"
+                            className="object-cover h-full rounded-lg shadow-md w-full "
+                        />
+                    </div>
+                    <div className="col-span-2 grid grid-cols-2 gap-4">
+                        <img
+                            src="https://i.ibb.co/JnDrMQW/pexels-nubia-navarro-nubikini-1522185.jpg"
+                            alt="Photo 2"
+                            className="object-cover h-64 rounded-lg shadow-md"
+                        />
+                        <img
+                            src="https://i.ibb.co/0yRVQ0s/pexels-pixabay-35967.jpg"
+                            alt="Photo 3"
+                            className="object-cover h-64 rounded-lg shadow-md"
+                        />
+                        <img
+                            src="https://i.ibb.co/k6zDM9t/pexels-hosein-ashrafosadat-243206.jpg"
+                            alt="Photo 4"
+                            className="object-cover h-64 rounded-lg shadow-md"
+                        />
+                        <img
+                            src="https://i.ibb.co/RywSDCk/pexels-pixabay-35619.jpg"
+                            alt="Photo 5"
+                            className="object-cover h-64 rounded-lg shadow-md"
+                        />
+                    </div>
                 </div>
+            </section>
+
+
+            {/* Shop by categories section */}
+            <section className='my-4 py-4'>
+                <h1 className='text-center text-2xl lg:text-5xl'>Categories</h1>
+                <p className='text-center text-neutral-500'>Unleash your kids favorite type of speedster</p>
+
+                <div className='my-4'>
+                    <Tabs>
+                        <TabList
+                            className='relative w-max mx-auto h-12 grid grid-cols-3 items-center px-[3px] rounded-full bg-gray-900/20 overflow-hidden shadow-2xl shadow-900/20 transition'
+                        >
+                            <Tab
+                                className='relative block h-10 px-6 py-1 lg:text-xl tab rounded-full'
+                                selectedClassName='bg-white rounded-full'
+
+                            >
+                                <span className='text-gray-800'>Sports Car</span>
+                            </Tab>
+                            <Tab
+                                className='relative block h-10 px-6 py-1 lg:text-xl tab rounded-full'
+                                selectedClassName='bg-white rounded-full'
+
+                            >
+                                <span className='text-gray-800'>Truck</span>
+                            </Tab>
+                            <Tab
+                                className='relative block h-10 px-6 py-1 lg:text-xl tab rounded-full'
+                                selectedClassName='bg-white rounded-full'
+
+                            >
+                                <span className='text-gray-800'>Police Car</span>
+                            </Tab>
+                        </TabList>
+
+                        <TabPanel
+                            selectedTabPanelClassName='bg-white font-bold p-4'
+                        >
+                            <h2>Any content 1</h2>
+                        </TabPanel>
+                        <TabPanel
+                            selectedTabPanelClassName='bg-white font-bold p-4'
+                        >
+                            <h2>Any content 2</h2>
+                        </TabPanel>
+                        <TabPanel
+                            selectedTabPanelClassName='bg-white font-bold p-4'
+                        >
+                            <h2>Any content 3</h2>
+                        </TabPanel>
+                    </Tabs>
+                </div>
+
             </section>
         </div>
     );
